76f6476330b15828fd32beb7e5c4dabc20fff546
Jamey <musoyou1085@gmail.com>
2024-07-14 01:14:29 +0900

    :construction_worker: ci : health 체크 controller 생성

---
+++ src/main/java/backend/like_house/global/common/RootController.java
@@ -0,0 +1,13 @@
+package backend.like_house.global.common;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm healthy!";
+    }
+}
