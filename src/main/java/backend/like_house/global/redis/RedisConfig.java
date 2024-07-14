9a2cd376fa9e7a3a028885407ac019d6cd232aa3
dabeann <dabin4078@daum.net>
2024-07-14 21:18:02 +0900

    ♻️  refactor : Remove useless import

--- src/main/java/backend/like_house/global/redis/RedisConfig.java
+++ src/main/java/backend/like_house/global/redis/RedisConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
