a8435180cfa0783d285c3bce8481b300eabb5879
dabeann <dabin4078@daum.net>
2024-07-19 23:14:31 +0900

    ♻️  Refactor : 접근제어자 추가

--- src/main/java/backend/like_house/domain/family_space/dto/FamilySpaceDTO.java
+++ src/main/java/backend/like_house/domain/family_space/dto/FamilySpaceDTO.java
@@ -3,6 +3,7 @@ package backend.like_house.domain.family_space.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,10 +17,10 @@ public class FamilySpaceDTO {
         public static class EnterFamilySpaceRequest {
             @NotNull
             @Schema(description = "입장할 유저 아이디", example = "1")
-            Long userId;
+            private Long userId;
             @NotNull
             @Schema(description = "입장할 가족 공간 아이디", example = "1")
-            Long familySpaceId;
+            private Long familySpaceId;
         }
     }
 
@@ -27,34 +28,35 @@ public class FamilySpaceDTO {
 
         @Builder
         @Getter
-        @NoArgsConstructor
-        @AllArgsConstructor
+        @NoArgsConstructor(access = AccessLevel.PROTECTED)
+        @AllArgsConstructor(access = AccessLevel.PROTECTED)
         public static class CheckFamilySpaceCodeResponse {
             @Schema(description = "초대 코드에 해당하는 가족 공간이 존재하면 true", example = "true")
-            Boolean isValid;
+            private Boolean isValid;
         }
 
         @Builder
         @Getter
-        @NoArgsConstructor
-        @AllArgsConstructor
+        @NoArgsConstructor(access = AccessLevel.PROTECTED)
+        @AllArgsConstructor(access = AccessLevel.PROTECTED)
         public static class NewFamilySpaceResponse {
             @Schema(description = "새로 만들어진 가족 공간 아이디", example = "1")
-            Long familySpaceId;
+            private Long familySpaceId;
             @Schema(description = "새로 만들어진 가족 공간 초대 코드", example = "A1b2C4")
-            String code;
-            LocalDateTime createdAt;
+            private String code;
+            @Schema(description = "가족 공간이 새로 만들어진 시간")
+            private LocalDateTime createdAt;
         }
 
         @Builder
         @Getter
-        @NoArgsConstructor
-        @AllArgsConstructor
+        @NoArgsConstructor(access = AccessLevel.PROTECTED)
+        @AllArgsConstructor(access = AccessLevel.PROTECTED)
         public static class EnterFamilySpaceResponse {
             @Schema(description = "입장한 유저 아이디", example = "1")
-            Long userId;
+            private Long userId;
             @Schema(description = "입장한 가족 공간 아이디", example = "1")
-            Long familySpaceId;
+            private Long familySpaceId;
         }
     }
 }
