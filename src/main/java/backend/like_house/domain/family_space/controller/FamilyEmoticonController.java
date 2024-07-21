74abe3c2bf4f5f933578f3b8e0f53981ab132d29
Jamey <musoyou1085@gmail.com>
2024-07-22 02:06:16 +0900

    📝 Docs : 가족 공간에 user가 포함되는 지 체크 swagger 추가

--- src/main/java/backend/like_house/domain/family_space/controller/FamilyEmoticonController.java
+++ src/main/java/backend/like_house/domain/family_space/controller/FamilyEmoticonController.java
@@ -4,7 +4,9 @@ import backend.like_house.domain.family_space.dto.FamilyEmoticonDTO;
 import backend.like_house.domain.family_space.dto.FamilyEmoticonDTO.FamilyEmoticonDetail;
 import backend.like_house.domain.family_space.dto.FamilyEmoticonDTO.FamilyEmoticonPreview;
 import backend.like_house.domain.family_space.dto.FamilyEmoticonDTO.FamilyEmoticonPreviewList;
+import backend.like_house.domain.user.entity.User;
 import backend.like_house.global.common.ApiResponse;
+import backend.like_house.global.security.annotation.LoginUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -25,39 +27,45 @@ public class FamilyEmoticonController {
     @Operation(summary = "가족 이모 티콘 전체 조회 API", description = "가족 이모 티콘 전체 조회 API입니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4003", description = "유저가 해당 가족 공간에 속해 있지 않습니다.")
     })
-    public ApiResponse<FamilyEmoticonPreviewList> getFamilyEmoticons() {
+    public ApiResponse<FamilyEmoticonPreviewList> getFamilyEmoticons(@LoginUser User user, @PathVariable Long familySpaceId) {
         return ApiResponse.onSuccess(null);
     }
 
-    @GetMapping("/family-emoticon/{familyEmoticonId}")
+    @GetMapping("/{familySpaceId}/family-emoticon/{familyEmoticonId}")
     @Operation(summary = "가족 이모 티콘 조회 API", description = "가족 이모 티콘 조회 API입니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_EMOTICON4001", description = "존재하지 가족 이모티콘 입니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_EMOTICON4001", description = "존재하지 가족 이모티콘 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4003", description = "유저가 해당 가족 공간에 속해 있지 않습니다.")
     })
-    public ApiResponse<FamilyEmoticonDetail> getFamilyEmoticon(@PathVariable Long familyEmoticonId) {
+    public ApiResponse<FamilyEmoticonDetail> getFamilyEmoticon(@LoginUser User user,@PathVariable Long familySpaceId, @PathVariable Long familyEmoticonId) {
         return ApiResponse.onSuccess(null);
     }
 
-    @PostMapping("/family-emoticon")
+    @PostMapping("/{familySpaceId}/family-emoticon")
     @Operation(summary = "가족 이모 티콘 생성 API", description = "가족 이모 티콘 생성 API입니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4003", description = "유저가 해당 가족 공간에 속해 있지 않습니다.")
     })
-    public ApiResponse<FamilyEmoticonPreview> createFamilyEmoticon(@RequestBody FamilyEmoticonDTO.CreateFamilyEmoticonRequest createFamilyEmoticonRequest) {
+    public ApiResponse<FamilyEmoticonPreview> createFamilyEmoticon(@LoginUser User user,@RequestBody FamilyEmoticonDTO.CreateFamilyEmoticonRequest createFamilyEmoticonRequest) {
         return ApiResponse.onSuccess(null);
     }
 
-    @DeleteMapping("/family-emoticon/{familyEmoticonId}")
+    @DeleteMapping("/{familySpaceId}/family-emoticon/{familyEmoticonId}")
     @Operation(summary = "가족 이모 티콘 삭제 API", description = "가족 이모 티콘 삭제 API입니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_EMOTICON4001", description = "존재하지 않는 가족 이모티콘 입니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4002", description = "존재하지 않는 가족 공간 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_EMOTICON4001", description = "존재하지 않는 가족 이모티콘 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "FAMILY_SPACE4003", description = "유저가 해당 가족 공간에 속해 있지 않습니다.")
     })
-    public ApiResponse<String> deleteFamilyEmoticon(@PathVariable Long familyEmoticonId) {
+    public ApiResponse<String> deleteFamilyEmoticon(@LoginUser User user,@PathVariable Long familySpaceId, @PathVariable Long familyEmoticonId) {
         return ApiResponse.onSuccess(null);
     }
 
