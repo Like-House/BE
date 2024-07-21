9a4876b8f96c5f962d607de5a40684085fa29f0f
dabeann <dabin4078@daum.net>
2024-07-22 02:44:11 +0900

    ♻️  Refactor : remove useless import

--- src/main/java/backend/like_house/domain/family_space/controller/FamilySpaceController.java
+++ src/main/java/backend/like_house/domain/family_space/controller/FamilySpaceController.java
@@ -16,7 +16,6 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
