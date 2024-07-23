a8037392f4e57e0f30f9fa6a9143237d08df6569
Jamey <musoyou1085@gmail.com>
2024-07-23 17:15:36 +0900

    Chore : 웹소켓 config 설정 및 관련 handler, interceptor 설정

---
+++ src/main/java/backend/like_house/global/socket/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package backend.like_house.global.socket;
+
+import backend.like_house.global.security.util.JWTUtil;
+import backend.like_house.global.socket.handler.JWTInterceptor;
+import backend.like_house.global.socket.handler.SocketUtil;
+import backend.like_house.global.socket.handler.TextHandler;
+import backend.like_house.global.socket.service.SocketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final SocketService socketService;
+    private final SocketUtil socketUtil;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new TextHandler(socketService, socketUtil),"/chat")
+                .addInterceptors(new JWTInterceptor(jwtUtil))
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
