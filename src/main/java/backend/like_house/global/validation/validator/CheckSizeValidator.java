edcd5d9929c40aa52d4d4f21eae82c00dab8cb2f
dabeann <dabin4078@daum.net>
2024-07-26 22:38:52 +0900

    ✨  Feature : Check page & size annotation

---
+++ src/main/java/backend/like_house/global/validation/validator/CheckSizeValidator.java
@@ -0,0 +1,27 @@
+package backend.like_house.global.validation.validator;
+
+import backend.like_house.global.error.code.status.ErrorStatus;
+import backend.like_house.global.validation.annotation.CheckSize;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckSizeValidator implements ConstraintValidator<CheckSize, Integer> {
+
+    @Override
+    public void initialize(CheckSize constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        if (value == null || value < 0){
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.INVALID_SIZE_NUMBER.toString())
+                    .addConstraintViolation();
+            return false;
+        }
+        return true;
+    }
+}
