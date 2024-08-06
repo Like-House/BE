535ceb5111cbcd0dcbb817d38ed13307dd0696ba
Jamey <musoyou1085@gmail.com>
2024-08-06 18:52:01 +0900

    :recycle: Refactor : 소켓 500에러 디스 코드 보내기 기능 추가

--- src/main/java/backend/like_house/global/socket/handler/CustomWebSocketExceptionHandler.java
+++ src/main/java/backend/like_house/global/socket/handler/CustomWebSocketExceptionHandler.java
@@ -1,8 +1,11 @@
 package backend.like_house.global.socket.handler;
 
+import backend.like_house.global.error.exception.alarmClient.dto.DiscordMessage;
+import backend.like_house.global.error.exception.alarmClient.service.DiscordClient;
 import backend.like_house.global.error.handler.ChatException;
 import backend.like_house.global.error.handler.ChatRoomException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketHandler;
@@ -10,11 +13,22 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.ExceptionWebSocketHandlerDecorator;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 @Slf4j
 public class CustomWebSocketExceptionHandler extends ExceptionWebSocketHandlerDecorator {
 
-    public CustomWebSocketExceptionHandler(WebSocketHandler delegate) {
+    private final DiscordClient discordClient;
+    private final Environment environment;
+
+    public CustomWebSocketExceptionHandler(WebSocketHandler delegate, DiscordClient discordClient, Environment environment) {
         super(delegate);
+        this.discordClient = discordClient;
+        this.environment = environment;
     }
 
     @Override
@@ -35,6 +49,9 @@ public class CustomWebSocketExceptionHandler extends ExceptionWebSocketHandlerDe
                 e.printStackTrace();
                 String errorMessage = "{\"isSuccess\": \"false\", \"code\": \"500\", \"message\": \"예외적 오류 서버 로그를 확인 해보 세요.\"}";
                 session.sendMessage(new TextMessage(errorMessage));
+                if (!Arrays.asList(environment.getActiveProfiles()).contains("local")) {
+                    discordClient.sendAlarm(createMessage(e));
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,5 +69,33 @@ public class CustomWebSocketExceptionHandler extends ExceptionWebSocketHandlerDe
         SocketUtil socketUtil = new SocketUtil();
         socketUtil.exitAllSessionChatRoom(session);
         super.afterConnectionClosed(session, closeStatus);
+    }
+
+    private DiscordMessage createMessage(Exception e) {
+        return DiscordMessage.builder()
+                .content("# 🚨 에러 발생 비이이이이사아아아앙")
+                .embeds(
+                        List.of(
+                                DiscordMessage.Embed.builder()
+                                        .title("ℹ️ 에러 정보")
+                                        .description(
+                                                "### 🕖 발생 시간\n"
+                                                        + LocalDateTime.now()
+                                                        + "\n"
+                                                        + "### 🔗 요청 URL\n"
+                                                        + "소켓 관련 에러"
+                                                        + "\n"
+                                                        + "### 📄 Stack Trace\n"
+                                                        + "```\n"
+                                                        + getStackTrace(e).substring(0, 1000)
+                                                        + "\n```")
+                                        .build()))
+                .build();
+    }
+
+    private String getStackTrace(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        return stringWriter.toString();
     }
 }
