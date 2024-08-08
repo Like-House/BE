4dd55f779ffa630467de65ec287f93baa6798776
Jamey <musoyou1085@gmail.com>
2024-08-08 16:45:09 +0900

    :sparkles: Feature : 푸시 알림 기능

---
+++ src/main/java/backend/like_house/global/firebase/FirebaseConfig.java
@@ -0,0 +1,26 @@
+package backend.like_house.global.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/firebase-admin-key.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
