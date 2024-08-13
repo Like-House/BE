38294d9a0b271204e2e176ebc02aa94f7f9c5cc5
Jamey <musoyou1085@gmail.com>
2024-08-13 21:32:45 +0900

    ♻️ Refactor : 알림 리턴 값 profile사진과 종류 추가

--- src/main/java/backend/like_house/domain/notification/converter/NotificationConverter.java
+++ src/main/java/backend/like_house/domain/notification/converter/NotificationConverter.java
@@ -1,10 +1,8 @@
 package backend.like_house.domain.notification.converter;
 
-import backend.like_house.domain.notification.dto.NotificationDTO;
 import backend.like_house.domain.notification.dto.NotificationDTO.NotificationResponseDTO;
 import backend.like_house.domain.notification.dto.NotificationDTO.NotificationResponseListDTO;
 import backend.like_house.domain.notification.entity.Notification;
-import backend.like_house.domain.post.entity.Post;
 import org.springframework.data.domain.Slice;
 
 import java.util.List;
@@ -25,12 +23,15 @@ public class NotificationConverter {
     }
 
     public static NotificationResponseDTO toNotificationResponseDTO(Notification notification) {
+
         return NotificationResponseDTO
                 .builder()
                 .title(notification.getTitle())
-                .sender(notification.getUser().getName())
+                .senderName(notification.getUser().getName())
+                .profileImage(notification.getUser().getProfileImage())
                 .createAt(notification.getDate())
                 .content(notification.getContent())
+                .notificationType(notification.getDtype())
                 .build();
     }
 }
